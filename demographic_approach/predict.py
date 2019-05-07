c309a2bc66c02c3d0c729b7b1eeda08c77a038f3
Michael Fine <mfine15@gmail.com>
2019-05-07 17:18:12 -0400

    implement simple demographic logistic regression prediction

---
+++ demographic_approach/predict.py
@@ -0,0 +1,36 @@
+# %%
+import numpy as np
+import pandas as pd
+import sklearn
+import sklearn.preprocessing
+from  sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import qgrid
+#%%
+df = pd.read_pickle("data/cases_justices_merged.pk")
+
+# df = df.dropna()
+# df.fillna(df.mode().iloc[0])
+X = df.drop(columns = ["justice", "justiceName", "justice_vote"])
+Y = df["justice_vote"]
+
+min_max_scaler = sklearn.preprocessing.StandardScaler()
+X = min_max_scaler.fit_transform(X)
+
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp.fit(X)
+X = imp.transform(X)
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+reg = linear_model.LogisticRegression(penalty="l1")
+
+reg.fit(X_train, Y_train)
+reg.score(X_test, Y_test)
+
+coeffs = pd.DataFrame(zip(reg.classes_, reg.coef_[0]), columns=["var", "coefficient"])
+coeffs
+coeffs.to_csv("logreg.csv")
+    
