26730ea494bfa7c1cae8e0f6e2cc3e05df2a3ffa
Jenny Huang <jhuange6@gmail.com>
2019-05-11 16:24:22 -0400

    Cleaned up ngram extraction code

--- nlp_approach/ngram_feature_extraction_tfidf.py
+++ nlp_approach/ngram_feature_extraction_tfidf.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from sklearn.impute import SimpleImputer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
 
 # Get file names
@@ -23,15 +24,19 @@ for f in files:
 print(justicenames)
 
 # vectorize 1-gram counts
-vectorizer = CountVectorizer(input="filename", stop_words="english", max_features=5000)
+vectorizer = CountVectorizer(input="filename", stop_words="english", max_features=5000, ngram_range=(1,5))
 X = vectorizer.fit_transform(new_files)
-Xdata = X.toarray()
-pickle.dump(Xdata, open("data/ngrams/1grams.p", "wb"))
 
 # print n-grams
-# print(vectorizer.get_feature_names())
-pickle.dump(vectorizer.get_feature_names(), open("data/ngrams/1grams_names.p", "wb"))
+print(vectorizer.get_feature_names())
+pickle.dump(vectorizer.get_feature_names(), open("data/ngrams/1to5grams_names.p", "wb"))
+
+transformer = TfidfTransformer()
+X = transformer.fit_transform(X)
+Xdata = X.toarray()
+pickle.dump(Xdata, open("data/ngrams/1to5grams.p", "wb"))
 print("Number of ngrams: ", len(Xdata[0]))
+
 
 # merge justice case data with n-grams from utterances
 jc_data = pickle.load(open("../demographic_approach/data/cases_justices_merged.pk", "rb" ))
@@ -41,7 +46,7 @@ ngram_df = ngram_df.T
 ngram_df["justiceName"] = ngram_df.index
 jc_data = jc_data.merge(ngram_df, on='justiceName')
 
-#pickle.dump(jc_data, open("data/case_justices_1grams_merged.p", "wb"))
+# pickle.dump(jc_data, open("data/case_justices_1grams_merged.p", "wb"))
 
 X = jc_data.drop(columns = ["justice", "justiceName", "justice_vote"]).values.astype(np.float64)
 Y = jc_data["justice_vote"].values.astype(np.int32)
@@ -49,7 +54,7 @@ imp = SimpleImputer(missing_values=np.nan, strategy='mean')
 imp.fit(X)
 X = imp.transform(X)
 
-np.save("data/case_justices_1grams_merged_X.npy", X)
-np.save("data/case_justices_1grams_merged_Y.npy", Y)
+np.save("data/case_justices_1to5grams_merged_X_idf.npy", X)
+np.save("data/case_justices_1to5grams_merged_Y_idf.npy", Y)
 
 print("Done")
